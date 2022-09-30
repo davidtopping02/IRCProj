adee48deb7b91103e915ce85703eed1984750662
David <2412905@dundee.ac.uk>
2022-09-30 12:01:32 +0100

    connected bot to miniircd server

--- botClient/bot.py
+++ botClient/bot.py
@@ -1,4 +1,12 @@
+# -----------------------------------------------------------
+# Creates a bot that acts as a client that can interact with an IRC server
+#
+# (C) David Topping, Christian Zlatanov, Caleb Harmon, Victor Iyida
+# https://github.com/davidtopping02/IRCProj
+# -----------------------------------------------------------
+
 # import network socket
+from email import message
 import socket
 
 
@@ -11,6 +19,7 @@ class BotClient:
         self.channel = "#test"
         self.nickName = nickName
         self.password = password
+        self.IRCSocket = None
 
         # this creates an IRC server object: other part of the project
         # ircServer = IRCServer()
@@ -19,8 +28,30 @@ class BotClient:
         # ircServer.connect(self.server, self.port, slef.channel, self.nickName, self.password)
         pass
 
-    def JoinMiniircdTESTING():
-        pass
+    def joinMiniircdTESTING(self):
+        """
+        Joins to a miniircd server, used for testing the bot functionallity but does it will connect to our own IRC server for submission
+        """
+
+        # creates socket object
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.IRCSocket:
+
+            # encode data for tramsmission
+            delimeter = " : ".encode()
+            user = self.nickName.encode()
+            message = "test123".encode()
+
+            # connect to IRC server
+            self.IRCSocket.connect(("10.0.24.4", self.port))
+
+            # send message
+            self.IRCSocket.sendall(user + delimeter + message)
+
+            while (True):
+                message = input().encode()
+                self.IRCSocket.sendall(user + delimeter + message)
+                data = self.IRCSocket.recv(1024)
+                print(f"Received {data!r}")
 
     def joinchannel():
         pass
@@ -28,8 +59,12 @@ class BotClient:
     def respondPrivMsg():
         pass
 
-    def respondChanCMD():
+    def respondChannelCMD():
         pass
+
+
+bot = BotClient("david", "test")
+bot.joinMiniircdTESTING()
 
 # conncet to server
 
