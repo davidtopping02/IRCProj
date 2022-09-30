4e82ecef38604f464f3112b7db4f131dbbedad59
David <2412905@dundee.ac.uk>
2022-09-30 11:08:17 +0100

    added skeleton for the bot client

---
+++ botClient/bot.py
@@ -0,0 +1,36 @@
+# import network socket
+from http import server
+import socket
+
+
+class BotClient:
+
+    def __init__(self, nickName, password):
+
+        # self.server =  #valid server
+        self.port = 6667
+        self.channel = "#test"
+        self.nickName = nickName
+        self.password = password
+
+        # this creates an IRC server object: other part of the project
+        # ircServer = IRCServer()
+
+    def joinServer(self):
+        # ircServer.connect(self.server, self.port, slef.channel, self.nickName, self.password)
+        pass
+
+    def joinchannel():
+        pass
+
+    def respondPrivMsg():
+        pass
+
+    def respondChanCMD():
+        pass
+
+# conncet to server
+
+# get response from server
+
+# so on...
