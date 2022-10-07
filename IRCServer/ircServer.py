8fa208a561d4e36f595a8185a0f14e83c1227c59
kekizl <tophack@outlook.com>
2022-10-07 13:12:01 +0100

    added USER function and comment updated

--- IRCServer/ircServer.py
+++ IRCServer/ircServer.py
@@ -26,7 +26,8 @@ class IRCServer:
     def command(self, response, user):
         processedMessage = response.split(" ")
         key = processedMessage[0]
-
+        
+        #if key is join, join channel
         if key == 'JOIN':
             print('joining')
             if (processedMessage[1] not in self.channelList):
@@ -36,6 +37,7 @@ class IRCServer:
                 channel.joinChannel(channel, user)
                 print("Successfully joined: " + channel.channelName)
 
+        #if key is part, leave channel
         if key == 'PART':
             print('quit')
             if (processedMessage[1] not in self.channelList):
@@ -43,16 +45,22 @@ class IRCServer:
             else:
                 leaveChannel(processedMessage, user)
                 print("Successfully disconnected")
+
         if key == 'MODE':
             print('mode')
+
+        #if key is nick, set nickname
         if key == 'NICK':
             user.set_nick(processedMessage[1])
             if user.nickName == processedMessage[1]:
                 print("Your new Nickname is: " + user.nickName)
+
+        #if key is USER, set fields of Client
         if key == 'USER':
-            print('user')
-            # user.set_nick(processedMessage[1])
-            # actualName = processedMessage[4]:1
+            user.userName = (processedMessage[1])
+            user.realName = processedMessage[4].replace(':','')
+            print('Your username: ' + user.userName)
+            print('Your realname: ' + user.realName)
         if key == 'PRIVMSG':
             print('private message')
             # sendPriv()
@@ -65,15 +73,21 @@ class IRCServer:
 
     # Function to start the server
     def startServer(self):
+
+        #makes new socket
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        #binds socket to ip
         s.bind((self.hostIP, self.hostPort))
         s.listen()
         print(f"Server Listening on {self.hostPort}")
 
         while True:
+            #client is connected by address
             conn, addr = s.accept()
             print(f"Connected by {addr}")
             self.connectedClients += 1
+            #makes new thread for client
             start_new_thread(self.multi_threaded_client,
                              (conn, self.connectedClients))
 
@@ -81,30 +95,17 @@ class IRCServer:
             client = Client(addr[1], addr[0])
             self.clientList.append(client)
 
-            # Printing entire client list
-            for i in range(len(self.clientList)):
-                self.clientList[i].test()
-
-            # self.clientList[1].test()
+        
             print('Clients Connected : ' + str(self.connectedClients))
             data = conn.recv(1024).decode('UTF-8')
+            data2 = data.split("\n")
             if not data:
                 break
 
-            # channel = Channel("#default")
-            # self.channelList.append(channel.channelName)
-
-            #channel.joinChannel(channel, client)
-            #channel.leaveChannel(channel, client)
-
-            print(data)
-            #self.command(data, client)
-
-            # testing for NICK
-            #dataArr= data.split(" ")
-            # if dataArr[0] == "NICK":
-            # self.clientList[0].set_nick("poop")
-            #print("new nick: " + self.clientList[0].nickName)
+            #prints and calls every line seperately
+            for x in range(len(data2)):
+                print("Server: " + data2[x])
+                self.command(data2[x], client)
 
         s.close()
     # Allows for multi-client connection, taken from https://www.positronx.io/create-socket-server-with-multiple-clients-in-python/
@@ -112,14 +113,18 @@ class IRCServer:
     def multi_threaded_client(self, connection, threadNum):
         #connection.send(str.encode('Server is working:'))
         while True:
+            #recieves data
             data = connection.recv(2048)
             #response = 'Server message: ' + data.decode('utf-8')
             # sends message to client
             response = data.decode('ascii')
+            response2 = response.split("\n")
 
-            print("Multi: " + response)
+            #prints and calls every line seperately
+            for x in range(len(response[2])):
+                print("Multi" + response2[x])
+                self.command(response2[x], self.clientList[threadNum-1])
 
-            self.command(response, self.clientList[threadNum-1])
             if not data:
                 break
             connection.sendall(str.encode(response))
@@ -131,15 +136,16 @@ class Client:
     def __init__(self, port, clientIP,):
         self.nickName = "test"
         self.realName = ""
-        self.user = ""
+        self.userName = ""
         self.port = port
         self.clientIP = clientIP
         self.connectedChannels = []
 
-    def test(self):
-        print(self.port)
-        print(self.clientIP)
-        print(self.nickName)
+    # used for testing new instances of client
+    #def test(self):
+     #   print(self.port)
+      #  print(self.clientIP)
+       # print(self.nickName)
 
     def set_nick(self, nick):
         self.nickName = nick
@@ -154,11 +160,13 @@ class Channel:
         self.channelClients = []
         #self.channelTopic = channelTopic
 
+    #join Channel function, currently under development
     def joinChannel(self, channel, client):
         self.channelClients.append(client)
-        for i in range(len(self.channelClients)):
-            print("YO Client Name" + self.channelClients[i].nickName)
+        #for i in range(len(self.channelClients)):
+         #   print("YO Client Name" + self.channelClients[i].nickName)
 
+    #leave Channel funciton, under development
     def leaveChannel(self, channel, client):
         print('leaving the channel')
 
